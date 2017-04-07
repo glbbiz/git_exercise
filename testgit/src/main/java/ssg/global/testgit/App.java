71dc09e3c30bcae256ae64633c0c362b56b35ee5
이호일 <user@AD0-P-D02121>
2017-04-07 09:20:37 +0900

    컨플릭트해결

--- testgit/src/main/java/ssg/global/testgit/App.java
+++ testgit/src/main/java/ssg/global/testgit/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!!, Hello World!!@@" );
         //System.out.println( "Hello World!!@@" );
+        System.out.println( "Hi!!!!!!!!" );
     }
 }
